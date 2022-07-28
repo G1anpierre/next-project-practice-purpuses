735003f21dd1dabaac2f72ed9f782e80c6a7e92b
Gianpierre Fernandez <fgianpierre@gmail.com>
2022-07-28 15:56:30 +0200

    client-side infinite-query and first set-up of getServerSide pagination

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,22 @@
 import '../styles/globals.css'
 import type {AppProps} from 'next/app'
+import {useState} from 'react'
 import {Layout} from '../components/Layout'
+import {ReactQueryDevtools} from '@tanstack/react-query-devtools'
+import {Hydrate, QueryClient, QueryClientProvider} from '@tanstack/react-query'
 
 function MyApp({Component, pageProps}: AppProps) {
+  const [queryClient] = useState(() => new QueryClient())
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Layout>
+          <Component {...pageProps} />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </Layout>
+      </Hydrate>
+    </QueryClientProvider>
   )
 }
 
--- pages/episodes/index.tsx
+++ pages/episodes/index.tsx
@@ -1,7 +1,75 @@
-import React from 'react'
+import React, {Fragment} from 'react'
+import {dehydrate, QueryClient, useInfiniteQuery} from '@tanstack/react-query'
+import {GetServerSideProps} from 'next'
+
+export type Episode = {
+  id: number
+  name: string
+  air_date: string
+  episode: string
+  characters: string[]
+  url: string
+  created: string
+}
 
 const Episodes = () => {
-  return <div>Episodes</div>
+  const {data, isLoading, fetchNextPage, hasNextPage} = useInfiniteQuery(
+    ['infiniteEpisodes'],
+    async ({pageParam = 1}) =>
+      await fetch(
+        `https://rickandmortyapi.com/api/episode/?page=${pageParam}`,
+      ).then(result => result.json()),
+    {
+      getNextPageParam: (lastPage, _allPages) => {
+        if (lastPage.info.next) {
+          return lastPage.info.next.split('=')[1]
+        }
+      },
+    },
+  )
+
+  const loadMoreEpisodes = () => {
+    fetchNextPage()
+  }
+
+  console.log('data', data)
+
+  return (
+    <>
+      <ul>
+        {isLoading ? (
+          <li>Loading...</li>
+        ) : (
+          data?.pages.map((group, index) => (
+            <Fragment key={index}>
+              {group.results.map((episode: Episode, index: number) => (
+                <li key={index}>{episode.name}</li>
+              ))}
+            </Fragment>
+          ))
+        )}
+      </ul>
+      {hasNextPage && (
+        <button onClick={loadMoreEpisodes}>Load more episodes</button>
+      )}
+    </>
+  )
 }
 
 export default Episodes
+
+// export const getServerSideProps: GetServerSideProps = async context => {
+//   let page = 2
+//   if (context.query.page) {
+//     page = parseInt(context.query.page)
+//   }
+//   const queryClient = new QueryClient()
+//   await queryClient.prefetchQuery(
+//     ['infiniteEpisodes', page],
+//     async () =>
+//       await fetch(`https://rickandmortyapi.com/api/episode/?page=${page}`).then(
+//         result => result.json(),
+//       ),
+//   )
+//   return {props: {dehydratedState: dehydrate(queryClient)}}
+// }
